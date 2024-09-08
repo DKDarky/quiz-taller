35f4bd485067ec64ccc77b4d12132a3fb5668e18
DKDarky <isazaarteagas@gmail.com>
2024-09-08 09:28:02 -0500

    unir la rama main con la rama avance2

--- quizProject/Proveedor.py
+++ quizProject/Proveedor.py
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 print("La montaña")
 print("Don julio")
 print("Lechera Laurita")
 print("Cafe Don Arturo")
 print("Arepa doña paisa")
+=======
+print("La montaña")
+print("Don julio")
+print("Lechera Laurita")
+print("Cafe Don Arturo")
+print("Arepa doña paisa")
+>>>>>>> avance2
